c61d708b5e8ae2d9fb19ec08ca373678c38adc22
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-06-18 09:32:51 +0000

    .

--- src/middlewares/Person.js
+++ src/middlewares/Person.js
@@ -1,19 +1,23 @@
 
 
 
-function Person(name) {
-    return (req, res, next) => {
-                if (name !== undefined) {
-                    req.myName = name;
-                    next();
-                } else {
-                    next(`you do not sent any name`);
-                }
-            }
+// function Person() {
+//     return (req, res, next) => {
+//                 if (name !== undefined) {
+//                     req.myName = name;
+//                     next();
+//                 } else {
+//                     next(`you do not sent any name`);
+//                 }
+//             }
    
+// }
+
+// module.exports = Person;
+
+module.exports=(req,res,next)=>{
+    req.myName=req.params.name;
+    next();
 }
-
-module.exports = Person;
-
 
 
--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
 'use strict'
 const express = require("express");
 const app=express();
-const stamper=require('./middlewares/stamper');
 const notFoundHandler=require('./handlers/404');
 const errorHandler=require('./handlers/500');
-const logger = require("./middlewares/logger");
 const Person = require('./middlewares/Person');
-const Validator = require('./middlewares/Validator');
-
 // app.use(cors());
 app.use(express.json());
-app.use(logger);
-
 app.get('/',(req,res)=>{
  res.send("hello world");
 
 })
 
-app.get('/data',stamper,sendRes)
+app.get('/data',sendRes)
 
 
 
@@ -35,18 +29,17 @@ app.get('/test', (req, res) => {
     res.send('hello----');
 });
 //if you sent Person() without any thing inside it will treger the error
-app.get('/person', Person("amro"), (req, res) => {
+// app.get('/person', Person("amro"), (req, res) => {
+//     res.send({
+//         message: "Person route",
+//         name: req.myName,
+//     });
+// });
+
+app.get('/person/:name', Person ,(req, res) => {
     res.send({
         message: "Person route",
         name: req.myName,
-    });
-});
-
-app.get('/Validator', Validator("amro"), (req, res) => {
-    res.send({
-        message: "Person route",
-        code:"200",
-        name: req.string,
     });
 });
 
